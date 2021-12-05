fe8f822b72bb9594f8427e61be9944dbaa4d188f
Henry Nguyen <huyhung1601@gmail.com>
2021-12-06 10:40:32 +1100

    Header Component and Tests

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './styles.scss'
 const Header = (props) => {
     return (
-        <header>
+        <header data-test="headerComponent">
             <div className="wrap" >
                 <div className="logo">
-                    <h6 className="logo">LOGO</h6>
+                    <h6 data-test="logoIMG">LOGO</h6>
                 </div>
             </div>
         </header>
